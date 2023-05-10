6df2d8b9ec1749eb202395b0c70c3e71f40167d2
Marc Boudreau <marc.boudreau@hashicorp.com>
2023-05-10 12:53:49 -0400

    change equality test to be based on Hash function [oss]

--- equater.go
+++ equater.go
@@ -9,25 +9,7 @@ import (
 type SimpleImageEquater struct{}
 
 func (s *SimpleImageEquater) Equal(left, right image.Image) bool {
-	leftBounds := left.Bounds()
-	rightBounds := right.Bounds()
-
-	if !leftBounds.Eq(rightBounds) {
-		return false
-	}
-
-	minPoint := leftBounds.Min
-	maxPoint := leftBounds.Max
-
-	for y := minPoint.Y; y < maxPoint.Y; y++ {
-		for x := minPoint.X; x < maxPoint.X; x++ {
-			if !ColorEqual(left.At(x, y), right.At(x, y)) {
-				return false
-			}
-		}
-	}
-
-	return true
+	return Hash(left) == Hash(right)
 }
 
 func NewSimpleImageEquater() *SimpleImageEquater {
